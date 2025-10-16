9a9bf454c5cfd3c1d48fb61c5858248b943fa8fd
orikuOlarotimi <rotimipraise07@gmail.com>
2025-10-16 19:45:52 +0100

    fixed issue of dotenv

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const rateLimit = require("express-rate-limit");
 require("dotenv").config(); 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(
